6ec27e6061a8dcf7446481dc9e9d63d85e0dbbe2
Samyam <139134689+samyam81@users.noreply.github.com>
2024-02-01 14:08:18 +0545

    Add files via upload

---
+++ LastWord.java
@@ -0,0 +1,12 @@
+public class LastWord {
+    public static void main(String[] args) {
+        String s="Hello World";
+        int length=lengthLastWord(s);
+        System.out.println(length);
+    }
+
+    private static int lengthLastWord(String s) {
+        String[] str = s.split(" ");
+        return str[str.length-1].length();
+    }
+}
