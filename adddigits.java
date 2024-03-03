fc4b00ac378dbd2103bf334d7359408ea0ce2c57
Samyam <139134689+samyam81@users.noreply.github.com>
2024-03-03 17:55:06 +0545

    Create adddigits.java

---
+++ adddigits.java
@@ -0,0 +1,20 @@
+public class adddigits {
+    public static void main(String[] args) {
+        int num=83;
+        int result= add(num);
+        System.err.println(result);
+    }
+
+    private static int add(int num) {
+    //    int sum=0;
+    //    while (num < 10) {
+    //         int temp=num%10;
+    //         sum=sum+temp;
+    //         num/=10;
+    //    }
+    //    return sum;
+       if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
+    }
+}
