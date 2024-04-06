ad1a8e112e0bd94ba8f1122b8401e8f638d3172c
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-06 11:58:57 +0545

    Create count1.java

---
+++ count1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class count1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a Decimal value");
+        int dec=scanner.nextInt();
+        int one=counting(dec);
+        System.out.println("the 1 in binary is "+one);
+    }
+
+    public static int counting(int dec){
+        int count=0;
+        long binary=DectoBinary(dec);
+        while (binary!=0) {
+            long temp= binary % 10;
+            if (temp==1) {
+                count++;
+            }
+            binary/=10;
+        }
+        return count;
+    }
+
+    public static long DectoBinary(int dec){
+        long binary = 0;
+        int base = 1;
+
+        while (dec > 0) {
+            int remainder = dec % 2;
+            binary += remainder * base;
+            dec /= 2;
+            base *= 10;
+        }
+
+        return binary;
+    }
+
+}
