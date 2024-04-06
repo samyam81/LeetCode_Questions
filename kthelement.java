613326d24b332d331166baf0267033ab17ad79f9
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-06 11:21:44 +0545

    Create kthelement.java

---
+++ kthelement.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class kthelement {
+    public static void main(String[] args) {
+            int number[]={2,3,6,7,8,9,5,1,4};
+            int k=3;
+            Arrays.sort(number);
+            System.out.println(number[k-1]);
+    }
+}
