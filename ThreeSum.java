a1b9b8ceab77f915b6a4ec45ab891a19261bf7e7
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-14 15:47:11 +0545

    Create ThreeSum.java

---
+++ ThreeSum.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array to easily handle duplicates
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int target = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    // Move pointers
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        ThreeSum solution = new ThreeSum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = solution.threeSum(nums);
+        System.out.println(result);
+    }
+}
