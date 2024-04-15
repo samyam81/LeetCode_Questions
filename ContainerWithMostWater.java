ae12fe659597d991af60adace5aeac03517211d1
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-15 08:06:54 +0545

    Create ContainerWithMostWater.java

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int a_pointer = 0;
+        int b_pointer = arr.length - 1;
+
+        int area = Integer.MIN_VALUE;
+
+        while (a_pointer <= b_pointer) {
+            if (arr[a_pointer] < arr[b_pointer]) {
+                area = Math.max(area, arr[a_pointer] * (b_pointer - a_pointer));
+                a_pointer++;
+            } else {
+                area = Math.max(area, arr[b_pointer] * (b_pointer - a_pointer));
+                b_pointer--;
+            }
+        }
+        System.out.println(area);
+    }
+}
