d456ef4a024f0809579cdff661973ae3aacf82bc
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-29 12:20:26 +0545

    Math

--- UglyNumber.kt
+++ UglyNumber.kt
@@ -1,9 +1,11 @@
 fun main() {
     println(isUglyNum(6))
+    println(isUglyNum(1))
     println(isUglyNum(14))
 }
 
 fun isUglyNum(number: Int): Boolean? {
+    if (number==1) return false
     val factors = intArrayOf(2, 3, 5)
     var num = number
 
