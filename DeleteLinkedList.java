557520da879c57ea4379479ba5227e8d99c33043
Samyam <139134689+samyam81@users.noreply.github.com>
2024-05-05 17:18:03 +0545

    Delete node at LinkedList.

---
+++ DeleteLinkedList.java
@@ -0,0 +1,28 @@
+public class DeleteLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printLinkedList(head);
+
+        ListNode nodeToDelete = head.next.next;
+        Solution solution = new Solution();
+        solution.deleteNode(nodeToDelete);
+
+        System.out.println("\nLinked List after deleting node with value 3:");
+        printLinkedList(head);
+    }
+
+    private static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
