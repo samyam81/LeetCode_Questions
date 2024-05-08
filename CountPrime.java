b432797cc1d6e4cee865b149369c18d0f58675a0
Samyam <139134689+samyam81@users.noreply.github.com>
2024-05-08 14:19:07 +0545

    Prime

---
+++ CountPrime.java
@@ -0,0 +1,24 @@
+public class CountPrime {
+
+    public int countPrimes(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
