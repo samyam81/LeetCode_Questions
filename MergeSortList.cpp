bd9029092998654aa3a2db16a36180feae2912eb
Samyam <139134689+samyam81@users.noreply.github.com>
2024-05-09 13:39:51 +0545

    Sort<LIst

---
+++ MergeSortList.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+
+        while (list1 && list2) {
+            if (list1->val > list2->val) {
+                cur->next = list2;
+                list2 = list2->next;
+            } else {
+                cur->next = list1;
+                list1 = list1->next;
+            }
+            cur = cur->next;
+        }
+
+        cur->next = list1 ? list1 : list2;
+
+        return dummy->next; 
+    }
+};
