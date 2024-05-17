0ed8bd2e504da594541fd349286d79542903b7f4
Samyam <samyam081@gmail.com>
2024-05-17 13:34:35 +0545

    Array.

---
+++ GuessGamer.java
@@ -0,0 +1,21 @@
+public class GuessGamer extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;
+            } else if (result == -1) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
