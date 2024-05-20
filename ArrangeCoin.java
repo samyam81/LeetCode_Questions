5363e3ef39bbf87ec87be270f990cca93d0b0265
Samyam <samyam081@gmail.com>
2024-05-20 12:26:33 +0545

    Breadth-First-Search

---
+++ ArrangeCoin.java
@@ -0,0 +1,29 @@
+public class ArrangeCoin{
+    public int arrangeCoins(int n) {
+        long left = 0;
+        long right = n;
+        
+        while (left <= right) {
+            long k = (left + right) / 2;
+            long curr = k * (k + 1) / 2;
+            
+            if (curr == n) {
+                return (int)k;
+            }
+            
+            if (curr < n) {
+                left = k + 1;
+            } else {
+                right = k - 1;
+            }
+        }
+        
+        return (int)right; // Return the number of complete rows
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 5;
+        System.out.println(solution.arrangeCoins(n)); // Output: 2
+    }
+}
