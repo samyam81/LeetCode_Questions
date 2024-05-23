d4946f79214cde37ae475bf3b737022a1f944b40
Samyam <samyam081@gmail.com>
2024-05-23 16:06:51 +0545

    HashMap (#7)

---
+++ FirstUniqueCharacter.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacter {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> charcounter=new HashMap<>();
+
+        for (char c : s.toCharArray())
+            charcounter.put(c, charcounter.getOrDefault(c,0)+1);           
+        
+        for (int i = 0; i < s.length(); i++) 
+            if (charcounter.get(s.charAt(i))==1) 
+                return i;
+            
+        return -1;
+    }
+}
