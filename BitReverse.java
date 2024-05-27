1af2da339718a55142a6d1b6dfec1edc60798f56
Samyam Subedi <samyam081@gmail.com>
2024-05-27 12:14:54 +0545

    Shift

---
+++ BitReverse.java
@@ -0,0 +1,11 @@
+public class BitReverse {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1; // Left-Shift 
+            result |= (n & 1); // and && or
+            n >>= 1; //Right-Shift
+        }
+        return result;
+    }
+}
