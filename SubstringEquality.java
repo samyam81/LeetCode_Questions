20f993463aa5450b421bcc5c49ada10dcb98ae93
Samyam Subedi <samyam081@gmail.com>
2024-05-28 10:05:23 +0545

    Sliding Window.

---
+++ SubstringEquality.java
@@ -0,0 +1,17 @@
+class SubstringEquality {
+
+  //Create the window from the right. Destroy from the left.....
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int ans = 0, window = 0, left = 0;
+        for (int right = 0; right < n; right++) {
+            window += Math.abs(s.charAt(right) - t.charAt(right));
+            while (window > maxCost) {
+                window -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans; 
+    }
+}
