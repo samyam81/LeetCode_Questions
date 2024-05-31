20324d2451dd618083b51ff6719487be38de9ca0
Samyam Subedi <samyam081@gmail.com>
2024-05-31 11:48:54 +0545

    XOR

---
+++ ComplementOFBase10Integer.java
@@ -0,0 +1,14 @@
+public class ComplementOFBase10Integer {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(bitwiseComplement(n));
+    }
+
+    public static int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        int numBits = (int) (Math.log(n) / Math.log(2)) + 1;
+        int mask = (1 << numBits) - 1;
+        int complement = n ^ mask;
+        return complement;
+    }
+}
